66ee8777263467d1bfe4a8258a966610c4dab842
TzuYi Chao <tzuyichao@mail2000.com.tw>
2015-12-29 08:50:13 +0800

    add merge function

--- ontology/exercise/jena3/fuseki-plugin-test/src/main/java/lab/ReplaceGraphServlet.java
+++ ontology/exercise/jena3/fuseki-plugin-test/src/main/java/lab/ReplaceGraphServlet.java
@@ -2,6 +2,8 @@ package lab;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +12,7 @@ import org.apache.jena.fuseki.server.DataAccessPoint;
 import org.apache.jena.fuseki.server.DataAccessPointRegistry;
 import org.apache.jena.fuseki.server.DataService;
 import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.GraphUtil;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.query.ReadWrite;
 import org.apache.jena.sparql.core.DatasetGraph;
@@ -20,6 +23,10 @@ import org.apache.jena.sparql.core.DatasetGraph;
  * @author tzuyichao
  */
 public class ReplaceGraphServlet extends HttpServlet {
+    private static final String METHOD_REPLACE = "REPLACE";
+    private static final String METHOD_MERGE = "MERGE";
+    private static final List<String> SUPPORT_METHODS = Arrays.asList(new String[] {METHOD_REPLACE, METHOD_MERGE});
+    
     private String makeRegistryKey(String name) {
         if(name.startsWith("/")) {
             return name;
@@ -34,11 +41,17 @@ public class ReplaceGraphServlet extends HttpServlet {
         String sourceDbName = request.getParameter("source");
         String targetDbName = request.getParameter("target");
         String named_graph_uri = request.getParameter("named_graph_uri");
+        String method = request.getParameter("method");
+        if(null == method) {
+            method = METHOD_REPLACE;
+        }
         DataAccessPointRegistry registry = DataAccessPointRegistry.get();
         
         if(null == sourceDbName || null == targetDbName || null == named_graph_uri) {
             // 這是不行的
             responseHelper(response, HttpServletResponse.SC_NOT_ACCEPTABLE, "source, target and named_graph_uri are requied parameter(s)");
+        } else if(!SUPPORT_METHODS.contains(method)) {
+            responseHelper(response, HttpServletResponse.SC_NOT_ACCEPTABLE, "the method only allowed [REPLACE] or [MERGE]");
         } else {
             String sourceDbKey = makeRegistryKey(sourceDbName);
             String targetDbKey = makeRegistryKey(targetDbName);
@@ -60,8 +73,14 @@ public class ReplaceGraphServlet extends HttpServlet {
         
                             Graph graph = sourceDatasetGraph.getDefaultGraph();
                             
-                            // 這個寫法是replace，要用</code>GraphUtil#addInto(target: Graph, source: Graph)</code>才會merge到target graph
-                            targetDatasetGraph.addGraph(NodeFactory.createURI(named_graph_uri), graph);
+                            if(METHOD_REPLACE.equalsIgnoreCase(method)) {
+                                // 這個寫法是replace，要用</code>GraphUtil#addInto(target: Graph, source: Graph)</code>才會merge到target graph
+                                targetDatasetGraph.addGraph(NodeFactory.createURI(named_graph_uri), graph);
+                            } else if (METHOD_MERGE.equalsIgnoreCase(method)) {
+                                // go merge
+                                Graph targetGraph = targetDatasetGraph.getGraph(NodeFactory.createURI(named_graph_uri));
+                                GraphUtil.addInto(targetGraph, graph);
+                            }
                             
                             response.setStatus(HttpServletResponse.SC_OK);
                         } finally {
