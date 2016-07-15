90ef6447d08f0dcab0e1370a2ef79b6c56c2d244
TzuYi Chao <tzuyichao@mail2000.com.tw>
2016-07-15 21:10:51 +0800

    java8 in action exercises

---
+++ java/java8-in-action/src/main/java/streams/Dish.java
@@ -0,0 +1,30 @@
+package streams;
+
+public class Dish {
+    private final String name;
+    private final Boolean vegetarian;
+    private final int calories;
+    private final Type type;
+    
+    public Dish(String name, Boolean vegetarian, int calories, Type type) {
+    	this.name = name;
+    	this.vegetarian = vegetarian;
+    	this.calories = calories;
+    	this.type = type;
+    }
+    
+    public String getName() {
+    	return name;
+    }
+    public Boolean getVegetarian() {
+    	return vegetarian;
+    }
+    public int getCalories() {
+    	return calories;
+    }
+    public Type getType() {
+    	return type;
+    }
+    
+    public enum Type { MEAT, FISH, OTHER }
+}
