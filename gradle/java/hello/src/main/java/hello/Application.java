6e49f5acf5dc2a3d4c1928329c111d45ee15baea
TzuYi Chao <tzuyichao@mail2000.com.tw>
2017-06-14 13:14:03 +0800

    add some test code

--- gradle/java/hello/src/main/java/hello/Application.java
+++ gradle/java/hello/src/main/java/hello/Application.java
@@ -3,6 +3,10 @@ package hello;
 import org.apache.commons.collections4.Bag;
 import org.apache.commons.collections4.bag.HashBag;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Application {
     public static void main(String[] args) {
         Bag<String> bag = new HashBag<>();
@@ -12,6 +16,18 @@ public class Application {
         System.out.println(String.format("Size: %d", bag.size()));
         for(String item:bag) {
             System.out.println(item);
+        }
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.update("password".getBytes(StandardCharsets.UTF_8));
+            byte[] hash = digest.digest();
+            StringBuffer hexString = new StringBuffer();
+            for(int i=0; i<hash.length; i++) {
+                hexString.append(Integer.toHexString(0xFF & hash[i]));
+            }
+            System.out.println(hexString.toString());
+        } catch(NoSuchAlgorithmException e) {
+            e.printStackTrace(System.out);
         }
     }
 }
